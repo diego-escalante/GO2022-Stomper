3f632b506fe84bb22be8f08f4d028a8323dba4bc
Diego Escalante <diego.escalante01@gmail.com>
2022-11-21 02:29:50 -0700

    Add saw and flag sprites

--- World/Goal.gd
+++ World/Goal.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+onready var animated_sprite := $AnimatedSprite as AnimatedSprite
+
 func _ready() -> void:
 	$Area2D.connect("body_entered", self, "_on_body_entered")
 	
 func _on_body_entered(body: Node) -> void:
 	Events.emit_signal("goal_reached", false)
+	animated_sprite.set_animation("Active")
